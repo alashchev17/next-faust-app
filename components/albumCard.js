d6aab2129d8261de11d5349ca76e6fa296dbe091
alashchev17 <andrew.lashchev15@gmail.com>
2024-05-09 16:32:55 +0200

    fix: vulnerabilities fix in package.json

--- components/albumCard.js
+++ components/albumCard.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function AlbumCard({ album }) {
   return (
     <Link href={`/albums/${album.slug}`}>
-      <img src={album?.albumFields.cover?.node?.mediaItemUrl} alt={album?.albumFields.cover?.node?.altText} />
+      <img className="albums__image" src={album?.albumFields.cover?.node?.mediaItemUrl} alt={album?.albumFields.cover?.node?.altText} />
     </Link>
   )
 }
